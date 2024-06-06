c02c5412385d5a9dc2fa56a29af57467500594b7
Andrea Zucca <andrea.zucca@spindox.it>
2024-06-06 13:39:43 +0200

    ci: fix jest

--- packages/flower-react/jest.config.ts
+++ packages/flower-react/jest.config.ts
@@ -8,5 +8,6 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'tsx', 'html'],
   coverageDirectory: '../../coverage/packages/flower-react',
   coverageProvider: 'v8',
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  transformIgnorePatterns: ['!/node_modules/(?!flat).+\\.js$']
 }
